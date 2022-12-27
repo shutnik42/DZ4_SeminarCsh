b3b56938f86e4c96939c6a63cbec40cd3995d804
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-27 14:58:09 +0300

    task025 solution1

--- TASK025/Program.cs
+++ TASK025/Program.cs
@@ -2,14 +2,13 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
 
 int Exponentiation(int numberA, int numberB){
   int result = 1;
   for(int i=1; i <= numberB; i++){
     result = result * numberA;
   }
-    // int result = Math.Pow(numberA, numberB);
+   
     return result;
 }
 
