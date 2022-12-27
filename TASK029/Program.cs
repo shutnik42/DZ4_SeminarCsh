87ced027b2f8e4d119876baa0a1fc3b2b1dc39cd
shutnik42 <118187272+shutnik42@users.noreply.github.com>
2022-12-27 15:51:16 +0300

    task029 solution

---
+++ TASK029/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: 
+// Напишите программу,
+// которая задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+//Console.Write(Console.ReadLine()!);
+
+int[]  array = new int[8];
+Console.Write("[");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    string elements = Convert.ToString(array[i]);
+    int convert = Convert.ToInt32(elements);
+    Console.Write(convert.ToString() + ", ");
+}
+Console.Write("]");
